80b612a22a65f76587cb8c72bca36584f1bcf7f8
yosa <45029926+yosa12978@users.noreply.github.com>
2020-05-23 11:47:53 +0300

    initial commit

---
+++ src/main/java/com/yosa/sponge/SpongeApplication.java
@@ -0,0 +1,13 @@
+package com.yosa.sponge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpongeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpongeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yosa/sponge/SpongeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yosa.sponge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpongeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
